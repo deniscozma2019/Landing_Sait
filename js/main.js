958975a3a909c2ee450640e1fb06b08846c6dcd0
denis-cozma <54998631+deniscozma2019@users.noreply.github.com>
2020-01-08 21:25:44 +0200

    corectare si adaugare top button

--- js/main.js
+++ js/main.js
@@ -58,3 +58,18 @@ $("#cssmenu").menumaker({
 })(jQuery);
 
 
+var btn = $('#button');
+
+$(window).scroll(function() {
+  if ($(window).scrollTop() > 300) {
+    btn.addClass('show');
+  } else {
+    btn.removeClass('show');
+  }
+});
+
+btn.on('click', function(e) {
+  e.preventDefault();
+  $('html, body').animate({scrollTop:0}, '300');
+});
+
